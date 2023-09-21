96f28682fdb88e88810f3d5f06a35082ea7cd681
krystian.antkowiak <krystian.antkowiak@xebia.com>
2023-09-21 12:05:39 +0200

    Setup codegen

--- src/api/grapql.ts
+++ src/api/grapql.ts
@@ -1,22 +1,36 @@
+import { type TypedDocumentString } from "@/gql/graphql";
+
 export type GraphQLResponse<T> =
 	| { data?: undefined; errors: { message: string }[] }
 	| { data: T; errors?: undefined };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function executeGraphql<T>(query: string, variables: Record<string, any>) {
+export const executeGraphql = async <TResult, TVariables>(
+	query: TypedDocumentString<TResult, TVariables>,
+	variables?: TVariables,
+): Promise<TResult> => {
 	if (!process.env.API_URL || !process.env.API_AUTH_TOKEN) {
 		throw new Error("Missing API configuration");
 	}
 
-	return fetch(process.env.API_URL, {
+	const res = await fetch(process.env.API_URL, {
 		method: "POST",
 		body: JSON.stringify({
 			query,
 			variables,
 		}),
 		headers: {
-			Authorization: process.env.API_AUTH_TOKEN,
+			Authorization: `Bearer ${process.env.API_AUTH_TOKEN}`,
 			"Content-Type": "application/json",
 		},
-	}).then((response) => response.json()) as Promise<GraphQLResponse<T>>;
-}
+	});
+
+	const graphqlResponse = (await res.json()) as GraphQLResponse<TResult>;
+
+	if (graphqlResponse.errors) {
+		throw TypeError(`GraphQL Error`, {
+			cause: graphqlResponse.errors,
+		});
+	}
+
+	return graphqlResponse.data;
+};
