5ca7ac5c96123ecb9a33f7346cbc7e87e1bcf2e0
krystian.antkowiak <krystian.antkowiak@xebia.com>
2023-09-25 00:13:47 +0200

    Fixes to tests - ver 3

--- src/app/categories/[slug]/[pageNo]/page.tsx
+++ src/app/categories/[slug]/[pageNo]/page.tsx
@@ -1,4 +1,5 @@
 import { notFound } from "next/navigation";
+import { type Metadata } from "next";
 import { ProductsPagination } from "@/ui/molecules/ProductsPagination";
 import { ProductsList } from "@/ui/organisms/ProductsList";
 import { getProductsList } from "@/api/products";
@@ -6,6 +7,18 @@ import { ProductsSection } from "@/ui/atoms/ProductsSection";
 import { PageHeading } from "@/ui/atoms/PageHeading";
 
 type CategoriesPageProps = { params: { pageNo: string; slug: string } };
+
+export const generateMetadata = async ({ params }: CategoriesPageProps): Promise<Metadata> => {
+	const { products } = await getProductsList({
+		pageNo: +params.pageNo,
+		where: { categories_every: { slug: params.slug } },
+	});
+
+	const category = products[0]?.categories?.[0];
+	return {
+		title: category ? category.name : "Category not found",
+	};
+};
 
 export default async function CategoriesPage({ params }: CategoriesPageProps) {
 	const { products, numberOfPages } = await getProductsList({
